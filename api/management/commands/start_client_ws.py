4f9b3a8cfa3f389b44508a9d6434596bf5c21cc6
danieldiasconcert <daniel.dias@concert.com.br>
2023-03-10 18:18:38 -0300

    upgrade requirementes e samples config

--- api/management/commands/start_client_ws.py
+++ api/management/commands/start_client_ws.py
@@ -2,8 +2,6 @@ from django.core.management.base import BaseCommand
 from api.client_websocket import main
 # Import script here
 
-
-
 class Command(BaseCommand):
     def add_arguments(self, parser):
         # parser.add_argument('--name', type=int)
